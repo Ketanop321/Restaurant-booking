3e3a980018f37b38b6dca844f7d5b35d8c97bbf1
Ketanop321 <ketanop473@gmail.com>
2024-11-30 00:25:49 +0530

    second commit added footer code

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import HeroSection from '../../components/HeroSection'
-
+import About from '../../components/About'
+import Footer from '../../components/Footer'
 const Home = () => {
   return (
     <>
     <HeroSection />
+    <About /> 
+    <Footer />
+    
     
     </>
   )
