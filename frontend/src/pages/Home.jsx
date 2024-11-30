227cbcf0f25c641d653ec4e9ca889b7b986f1aaa
Ketanop321 <ketanop473@gmail.com>
2024-11-30 23:15:30 +0530

    added pages

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,14 +2,26 @@ import React from 'react'
 import HeroSection from '../../components/HeroSection'
 import About from '../../components/About'
 import Footer from '../../components/Footer'
+import Qualities from '../../components/Qualities'
+import Menu from '../../components/Menu'
+import WhoAreWe from '../../components/WhoAreWe'
+import Team from '../../components/Team'
+import Reservation from '../../components/Resevation'
+
 const Home = () => {
   return (
     <>
     <HeroSection />
     <About /> 
+    <Qualities />
+    <Menu />
+    <WhoAreWe />
+    <Team />
+    <Reservation />
     <Footer />
-    
-    
+
+
+
     </>
   )
 }
